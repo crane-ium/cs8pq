cfd0e01de4b5da28a29695b32ca8d6eb6b123409
Stephen C <cranegames@gmail.com>
2018-10-10 10:58:03 -0700

    Setup priority queue layout

--- priority_queue.h
+++ priority_queue.h
@@ -1,4 +1,21 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
+#include "heap.h"
+
+using namespace std;
+
+template<class T>
+struct info{
+
+};
+
+template<class T>
+class priority_queue{
+
+};
+
+
+
+
 #endif // PRIORITY_QUEUE_H
